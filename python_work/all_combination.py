d9d6be65a6d3a52cd08526e987052cf84f032aa3
kenki-seki <pontawantym.obaka@gmail.com>
2018-11-25 00:25:57 +0900

    second commit

--- python_work/all_combination.py
+++ python_work/all_combination.py
@@ -1,6 +1,9 @@
+import time
 import itertools
-input_lines = input().split()
-result = 0
-for i in itertools.permutations(input_lines):
-    print(i)
-    
+while True:
+    input_lines = input().split()
+    if (input_lines[0] == 'exit'):
+        break
+    result = 0
+    for i in itertools.permutations(input_lines):
+        print(i)
