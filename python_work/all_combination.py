8d63ba9d8700bc824017847f513e49b110680154
Seki-FoolishNeet <pontawantym.obaka@gmail.com>
2018-11-25 00:28:44 +0900

    Update all_combination.py

--- python_work/all_combination.py
+++ python_work/all_combination.py
@@ -1,9 +1,7 @@
-import time
 import itertools
 while True:
     input_lines = input().split()
     if (input_lines[0] == 'exit'):
         break
-    result = 0
     for i in itertools.permutations(input_lines):
         print(i)
